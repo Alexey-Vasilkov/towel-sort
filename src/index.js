636e8634579e5c01f31f9b708f9a07c70d9eed07
Alexey-Vasilkov <alexey.a.vasilkov@gmail.com>
2020-10-19 19:27:46 +0300

    fix: logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) { 
-  if (matrix == null) return [];
+  let result = []
+  if (matrix == null) return result;
 
-  let result = matrix.reduce((accumulator, current, index) => {
+  result = matrix.reduce((accumulator, current, index) => {
     if (index % 2 == 0) accumulator.push(...current)
     else accumulator.push(...current.reverse())
     return accumulator;
